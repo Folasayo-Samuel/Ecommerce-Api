ff63a754a4c1a741dfae30f05e98442edaf2e504
Folasayo Samuel <folasayosamuel@gmail.com>
2023-02-15 00:20:33 +0100

    update

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,4 +1,4 @@
-const Order = require("../models/Order");
+const Order = require("../models/order");
 
 exports.getAllOrders = async (req, res) => {
   try {
--- utils/logging.js
+++ utils/logging.js
@@ -2,13 +2,16 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function () {
-  winston.handleExceptions(
+  winston.exceptions.handle(
     new winston.transports.Console({
-      colorize: true,
-      prettyPrint: true,
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
     }),
     new winston.transports.File({
       filename: "uncaughtException.log",
+      format: winston.format.json(),
     })
   );
 
@@ -16,7 +19,10 @@ module.exports = function () {
     throw ex;
   });
 
-  winston.add(winston.transports.File, {
-    filename: "logfile.log",
-  });
+  winston.add(
+    new winston.transports.File({
+      filename: "logfile.log",
+      format: winston.format.json(),
+    })
+  );
 };
